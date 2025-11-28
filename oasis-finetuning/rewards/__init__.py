d9e3d67e43d71c6ec3c0dd0ec254042065de3531
rijuld <errijuldahiya@gmail.com>
2025-11-28 12:40:45 -0500

    a

--- oasis-finetuning/rewards/__init__.py
+++ oasis-finetuning/rewards/__init__.py
@@ -4,7 +4,7 @@ Ground-truth-free reward functions for Oasis RL finetuning.
 
 from .game_world_score import GameWorldScoreReward
 from .inverse_kinematics import InverseKinematicsReward
-from .temporal_consistency import TemporalConsistencyReward
+from .temporal_consistency import TemporalConsistencyRewardV2 as TemporalConsistencyReward
 from .aesthetic_quality import AestheticQualityReward
 
 __all__ = [
